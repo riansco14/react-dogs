9081c9d675462f7a988d3c3579b3d0779c88611b
riansco14 <rian.rabelo@ufrpe.br>
2022-08-15 01:28:51 -0300

    route account user

---
+++ src/views/User/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Feed } from '../../components/Feed'
+import { UserHeader } from '../../components/UserHeader'
+import { UserPhotoPost } from '../../components/UserPhotoPost'
+import { UserStatistic } from '../../components/UserStatistic'
+
+import {Container} from './styles'
+
+export function User() {
+    return (
+        <Container>
+            <UserHeader />
+            <Routes>
+                <Route path='/' element={<Feed />} />
+                <Route path='/post' element={<UserPhotoPost />} />
+                <Route path='/statistic' element={<UserStatistic />} />
+            </Routes>
+        </Container>
+    )
+}
