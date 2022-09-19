31f6296deb95919c9debb52c03cb0819b433c291
Rian <rian.azevedo@qintess.com>
2022-09-19 15:40:59 -0300

    feedModal

---
+++ src/components/FeedModal/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {Container} from './styles'
+
+export function FeedModal() {
+    const { } = useQuery(['photo', photo.id], () => [])
+    
+    return (
+        <Container>
+
+        </Container>
+    )
+}
