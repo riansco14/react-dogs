1ee6b7e550b98f0147c5de7cf37add9880899667
Rian <rian.azevedo@qintess.com>
2022-10-21 12:14:35 -0300

    Add react-query config

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
@@ -11,33 +11,39 @@ import { NotFound } from './views/NotFound/';
 import { ProtectedRoute } from './components/Helpers/ProtectedRoute';
 import { User } from './views/User';
 import { AuthLoading } from 'components/Helpers/AuthLoading';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
-          <ProviderAuth>
-            <GlobalStyle />
-            <Header />
-            <AuthLoading>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="login/*" element={<ProtectedRoute userExists={true} redirectPath="/account" />} >
-                  <Route index element={<Login />} />
-                </Route>
-                <Route element={<ProtectedRoute />} >
-                  <Route path="account/*" element={<User />} />
-                </Route>
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </AuthLoading>
-            <Footer />
-          </ProviderAuth>
+          <QueryClientProvider client={queryClient}>
+            <ProviderAuth>
+              <GlobalStyle />
+              <Header />
+              <AuthLoading>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="login/*" element={<ProtectedRoute userExists={true} redirectPath="/account" />} >
+                    <Route index element={<Login />} />
+                  </Route>
+                  <Route element={<ProtectedRoute />} >
+                    <Route path="account/*" element={<User />} />
+                  </Route>
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </AuthLoading>
+              <Footer />
+            </ProviderAuth>
+          </QueryClientProvider>
         </ThemeProvider>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
